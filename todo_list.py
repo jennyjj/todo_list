b2805eee390f52335101ac96c11aa2094afb8884
Jenny <kang.je@gmail.com>
2017-05-02 00:39:59 -0700

    adding more functionality

--- todo_list.py
+++ todo_list.py
@@ -12,20 +12,54 @@ You can run the script in your Terminal at any time using the command:
 def add_to_list(my_list):
     """Takes user input and adds it as a new item to the end of the list."""
 
-    item = raw_input("What would you like to add to the list?")
+    item = (raw_input("What would you like to add to the list? ")).lower()
+    
     if len(my_list) == 0:
         my_list.append(item)
-    for i in my_list:
-        if i == item:
-            print "You already have that item. Pick another item."
-        else:
-            my_list.append(item)
+    else:
+        for i in my_list:
+            if i == item:
+                print "This is already on the list."
+                print i, item            # if item == i:
+                # print "You already have that."
+            else:
+                my_list.append(item)
+
+    print my_list
 
 def view_list(my_list):
     """Print each item in the list."""
 
-    for item in range(len(my_list)):
-        print my_list[item]
+    for item in my_list:
+        print item
+
+    preference = (raw_input("Would you like to see the list in alphabetical order? ")).lower()
+    if preference == "yes":
+        for item in my_list:
+            print item
+    else:
+        print "Ok."
+
+    option = (raw_input("Would you like to see a certain number of items? ")).lower()
+    if option == "yes":
+        view_certain_number_in_list(my_list)
+    else:
+        print "Ok."
+
+    another_option = (raw_input("Would you like to see a portion of the list? ")).lower()
+    if another_option == "yes":
+        view_slices_of_list(my_list)
+
+def view_certain_number_in_list(my_list):
+    number = int(raw_input("How many items do you want to see? "))
+    for i in range(number):
+        print my_list[i]
+
+def view_slices_of_list(my_list):
+    portion_start = int(raw_input("At which index do you want to start? "))
+    portion_end = int(raw_input("At which index do you want to end? "))
+    for i in range(portion_start, portion_end + 1):
+        print my_list[i]
 
 def delete_from_beginning_of_list(my_list):
     if len(my_list) == 0:
